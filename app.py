ae8344eff9ef52465d7af5b2d078318d27097e98
Alban Leandri <alban.leandri@gmail.com>
2016-11-20 23:58:05 +0700

    app bucket with like feature

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, json, request, session, redirect
 from flaskext.mysql import MySQL
 from werkzeug import generate_password_hash, check_password_hash
+import uuid
+import os
 
 app = Flask(__name__)
 app.secret_key = 'why would I tell you my secret key?'
@@ -12,6 +14,7 @@ app.config['MYSQL_DATABASE_USER'] = 'AlbanLeandri'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Alban1234'
 app.config['MYSQL_DATABASE_DB'] = 'BucketList'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['UPLOAD_FOLDER'] = 'static/Uploads'
 mysql.init_app(app)
 
 
@@ -24,56 +27,7 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 	
-@app.route('/showSignin')
-def showSignin():
-    if session.get('user'):
-        return render_template('userHome.html')
-    else:
-        return render_template('signin.html')
-		
 
-@app.route('/userHome')
-def userHome():
-    if session.get('user'):
-        return render_template('userHome.html')
-    else:
-        return render_template('error.html',error = 'Unauthorized Access')
-		
-@app.route('/logout')
-def logout():
-    session.pop('user',None)
-    return redirect('/')	
-
-
-		
-@app.route('/validateLogin',methods=['POST'])
-def validateLogin():
-    try:
-        _username = request.form['inputEmail']
-        _password = request.form['inputPassword']
-  
-        # connect to mysql
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('sp_validateLogin',(_username,))
-        data = cursor.fetchall()
- 
-        if len(data) > 0:
-            if check_password_hash(str(data[0][3]),_password):
-                session['user'] = data[0][0]
-                return redirect('/userHome')
-            else:
-                return render_template('error.html',error = 'Wrong Email address or Password.')
-        else:
-            return render_template('error.html',error = 'Wrong Email address or Password.')
- 
-    except Exception as e:
-        return render_template('error.html',error = str(e))
-    finally:
-        cursor.close()
-        con.close()
-		
-		
 @app.route('/signUp',methods=['POST', 'GET'])
 def signUp(): 
     try:
@@ -105,6 +59,287 @@ def signUp():
     finally:
         cursor.close() 
         conn.close()
+
+
+@app.route('/showSignin')
+def showSignin():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('signin.html')
+		
+		
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+  
+        # connect to mysql
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+ 
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/showDashboard')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+ 
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
+	
+	
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')	
+		
+
+		
+@app.route('/showDashboard')
+def showDashboard():
+    return render_template('dashboard.html')		
+		
+		
+		
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+		
+		
+@app.route('/showAddWish')
+def showAddWish():
+    return render_template('addWish.html')
+	
+
+@app.route('/addWish',methods=['POST'])
+def addWish():
+    try:
+        if session.get('user'):
+            _title = request.form['inputTitle']
+            _description = request.form['inputDescription']
+            _user = session.get('user')
+            if request.form.get('filePath') is None:
+                _filePath = ''
+            else:
+                _filePath = request.form.get('filePath')
+            if request.form.get('private') is None:
+                _private = 0
+            else:
+                _private = 1
+            if request.form.get('done') is None:
+                _done = 0
+            else:
+                _done = 1
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_addWish',(_title,_description,_user,_filePath,_private,_done))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'An error occurred!')
+
+        else:
+            return render_template('error.html',error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        conn.close()
+		
+
+@app.route('/getWish')
+def getWish():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+ 
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('sp_GetWishByUser',(_user,))
+            wishes = cursor.fetchall()
+ 
+            wishes_dict = []
+            for wish in wishes:
+                wish_dict = {
+					'Id': wish[0],
+					'Title': wish[1],
+					'Description': wish[2],
+					'Date': wish[4]}
+                wishes_dict.append(wish_dict)
+ 
+            return json.dumps(wishes_dict)
+        else:
+            return render_template('error.html', error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html', error = str(e))
+		
+
+@app.route('/getWishById',methods=['POST'])
+def getWishById():
+    try:
+        if session.get('user'):
+ 
+            _id = request.form['id']
+            _user = session.get('user')
+ 
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetWishById',(_id,_user))
+            result = cursor.fetchall()
+ 
+            wish = []
+            wish.append({'Id':result[0][0],'Title':result[0][1],'Description':result[0][2],'FilePath':result[0][3],'Private':result[0][4],'Done':result[0][5]})
+ 
+            return json.dumps(wish)
+        else:
+            return render_template('error.html', error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+
+
+
+@app.route('/getAllWishes')
+def getAllWishes():
+    try:
+        if session.get('user'):
+             
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_GetAllWishes')
+            result = cursor.fetchall()
+         
+ 
+         
+            wishes_dict = []
+            for wish in result:
+                wish_dict = {
+                        'Id': wish[0],
+                        'Title': wish[1],
+                        'Description': wish[2],
+                        'FilePath': wish[3]}
+                wishes_dict.append(wish_dict)       
+ 
+            
+ 
+            return json.dumps(wishes_dict)
+        else:
+            return render_template('error.html', error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+
+		
+@app.route('/addUpdateLike',methods=['POST'])
+def addUpdateLike():
+    try:
+        if session.get('user'):
+            _wishId = request.form['wish']
+            _like = request.form['like']
+            _user = session.get('user')
+            
+ 
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_AddUpdateLikes',(_wishId,_user,_like))
+            data = cursor.fetchall()
+ 
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'status':'OK'})
+            else:
+                return render_template('error.html',error = 'An error occurred!')
+ 
+        else:
+            return render_template('error.html',error = 'Unauthorized Access')
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        conn.close()		
+		
+		
+		
+
+@app.route('/updateWish', methods=['POST'])
+def updateWish():
+    try:
+        if session.get('user'):
+            _user = session.get('user')
+            _title = request.form['title']
+            _description = request.form['description']
+            _wish_id = request.form['id']
+            _filePath = request.form['filePath']
+            _isPrivate = request.form['isPrivate']
+            _isDone = request.form['isDone']
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_updateWish',(_title,_description,_wish_id,_user,_filePath,_isPrivate,_isDone))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'status':'OK'})
+            else:
+                return json.dumps({'status':'ERROR'})
+    except Exception as e:
+        return json.dumps({'status':'Unauthorized access'})
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/deleteWish',methods=['POST'])
+def deleteWish():
+    try:
+        if session.get('user'):
+            _id = request.form['id']
+            _user = session.get('user')
+ 
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_deleteWish',(_id,_user))
+            result = cursor.fetchall()
+ 
+            if len(result) is 0:
+                conn.commit()
+                return json.dumps({'status':'OK'})
+            else:
+                return json.dumps({'status':'An Error occured'})
+        else:
+            return render_template('error.html',error = 'Unauthorized Access')
+    except Exception as e:
+        return json.dumps({'status':str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+
+		
+		
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    # file upload handler code will be here
+	if request.method == 'POST':
+		file = request.files['file']
+		extension = os.path.splitext(file.filename)[1]
+		f_name = str(uuid.uuid4()) + extension
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+		return json.dumps({'filename':f_name})
+	
 
 if __name__ == "__main__":
     app.run(port=port_number)
